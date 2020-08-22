50f81cf437709e18c77de651bf1c4bbfd3fa6a28
Mochichi2003 <44772513+Mochichi2003@users.noreply.github.com>
2020-08-22 22:49:31 +0900

    Revert "iai"
    This reverts commit ea56f78e2ff4d48df41f64ed7cadb8188cbdfe0e.
    
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    
    On branch master
    Your branch is ahead of 'origin/master' by 3 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   index.js
    
    Changes not staged for commit:
    	modified:   app.js

--- index.js
+++ index.js
@@ -260,10 +260,12 @@ getdata();
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 // app.listen(port, () => console.log(`Example app listening on port port!`))
-
-  const express = require("express");
-  const app = express();
-  const port = process.env.PORT || 300;
-  app.get("/", (req, res) => res.send("Hello World!"));
-  app.listen(port, () => console.log(`Example app listening on port port!`));
-
+try {
+  // const express = require("express");
+  // const app = express();
+  // const port = process.env.PORT || 3000;
+  // app.get("/", (req, res) => res.send("Hello World!"));
+  // app.listen(port, () => console.log(`Example app listening on port port!`));
+} catch (error) {
+  console.log(error);
+}
